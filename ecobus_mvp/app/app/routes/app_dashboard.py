db1be4c3846e68e14ff0785fa8004b92ab1e4678
fernandoastorga-ship-it <fernando.astorga@rentabuses.cl>
2026-03-20 18:39:43 -0300

    Update app_dashboard.py

--- ecobus_mvp/app/app/routes/app_dashboard.py
+++ ecobus_mvp/app/app/routes/app_dashboard.py
@@ -1,1 +1,12 @@
+from fastapi import APIRouter
 
+router = APIRouter(prefix="/app/dashboard", tags=["App Dashboard"])
+
+
+@router.get("/")
+def get_dashboard():
+    return {
+        "ok": True,
+        "message": "Dashboard passenger app funcionando 🚀",
+        "module": "app_dashboard",
+    }
